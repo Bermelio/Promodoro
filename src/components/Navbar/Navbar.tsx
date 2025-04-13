2dce1012d4d28eeb9c019a867b10e4ec970a3531
Bermelio <facundoperalta258@gmail.com>
2025-04-13 01:31:49 -0300

    My new branch

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ function Navbar(){
 
     return (
     <>
-        <nav>
+        <nav className="">
             <h1 className="bg-[#536878] justify-center flex p-3.5 font-bold text-2xl">Promodoro</h1>
         </nav>
     </>
