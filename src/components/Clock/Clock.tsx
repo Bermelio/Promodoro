7139c760244983fee4d7c0647d75c94c7ec36f8d
Bermelio <facundoperalta258@gmail.com>
2025-04-23 19:41:26 -0300

    some changes 1.0 maybe

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,15 +1,40 @@
-import { motion } from 'framer-motion';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import StopStart from './StopStart'
+import { useState } from 'react';
 
 function Clock(){
+    
+    const Promodoro25 = 25 * 60; 
+    
+    const [secondLeft, setSecondLeft] = useState(Promodoro25);
+    
+    const minLeft = Promodoro25/60;
+
 
     return(
         <>
-            <div className='flex justify-center items-center'>   
-                <motion.div/>
+            <div className='flex justify-center items-center m-10 flex-col'>   
+                <CircularProgressbar
+                value={0.5}
+                text={`${minLeft}'`}
+                maxValue={25}
+                styles={buildStyles({
+                    rotation: 1,
+                    strokeLinecap: 'butt',
+                    trailColor: '#d1d1d1',
+                    textColor: '#567853',
+                    pathColor: '#567853'
+                })   
+                }
+                />;
+
+                <StopStart />
             </div>
         </>
     )
 }
+
 
 export default Clock;
 
---
+++ src/components/Clock/StopStart.tsx
@@ -0,0 +1,13 @@
+
+function StopStart(){
+    return(
+        <>
+            <div className='flex '>
+                <button className='m-2 p-3 bg-[#536878] rounded-lg shadow-md hover:bg-[#5d7e94] text-[#EAE0C8] transition'>Start</button>
+                <button className='m-2 p-3 bg-[#536878] rounded-lg shadow-md hover:bg-[#5d7e94] text-[#EAE0C8] transition'>Reset</button>
+            </div> 
+        </>
+    )
+}             
+
+export default StopStart;
